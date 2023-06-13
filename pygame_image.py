74c22b7c1bdd61e5771435a01cc7398e25505654
KashiKun <c0b2204288@edu.teu.ac.jp>
2023-06-13 15:52:38 +0900

    practice 5

--- pygame_image.py
+++ pygame_image.py
@@ -7,17 +7,23 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
-    koton = pg.image.load("ex01/fig/3.png")
-    koton = pg.transform.flip(koton, True, False)
+    kotons = list()
+    koton = pg.transform.flip(pg.image.load("ex01/fig/3.png"), True, False)
+    kotons.append([koton,0 , 0])
+    kotons.append([pg.transform.rotozoom(koton,10,1), 300, 200])
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
-        screen.blit(koton, (0, 0))
+        screen.blit(bg_img, [-tmr, 0])
+        screen.blit(bg_img, (-tmr+1600, 0))
+        koton = kotons[(tmr//4)%2]
+        screen.blit(koton[0], (300, 200))
         pg.display.update()
-        tmr += 1        
+        tmr += 1
+        if tmr >= 1600:
+            tmr = 0
         clock.tick(10)
 
 
