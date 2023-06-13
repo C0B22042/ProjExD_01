6b1968fc2e65a5b5b06b5b22be9a9f69e7991367
KashiKun <c0b2204288@edu.teu.ac.jp>
2023-06-13 16:25:39 +0900

    演習課題２

--- pygame_image.py
+++ pygame_image.py
@@ -7,6 +7,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img, True, False)
     kotons = list()
     koton = pg.transform.flip(pg.image.load("ex01/fig/3.png"), True, False)
     for angle in range(0,10,1):
@@ -17,16 +18,16 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [-tmr, 0])
-        screen.blit(bg_img, (-tmr+1600, 0))
+        screen.blit(bg_img2, (-tmr+1600, 0))
+        screen.blit(bg_img, (-tmr+3200, 0))
         if ((tmr%(len(kotons)*2))//len(kotons)) == 0: k_tmr = tmr%len(kotons)
         else: k_tmr = -tmr%len(kotons)-1
-        print((tmr%(len(kotons)*2)))
         koton = kotons[k_tmr]
         screen.blit(koton, (300, 200))
         pg.display.update()
         tmr += 1
-        if tmr >= 1600: tmr = 0
-        clock.tick(10)
+        if tmr >= 3200: tmr = 0
+        clock.tick(100)
 
 
 if __name__ == "__main__":
